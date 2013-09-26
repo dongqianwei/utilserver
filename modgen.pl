4203dd2c7f3cc9065a151a4494b64fb6be402e4b
dongqianwei <dongqianwei@foxmail.com>
2013-09-26 23:00:50 +0800

    genmod

--- modgen.pl
+++ modgen.pl
@@ -1,8 +1,24 @@
 use Mojo::Template;
+use Getopt::Long;
 use strict;
 
+my ($mod, @fields);
+
+GetOptions('name=s' => \$mod,
+           'fields=s{1,}' => \@fields);
+
+my $help = <<H;
+    this tool is used to create Plugin Template
+    sample:
+        perl modgen --name PluginName --fields field1 field2 field3
+
+按任意键退出。。。
+H
+
+do { print $help; <>; exit } unless defined $mod;
+
 my $tp = <<'T';
-% my $mod = shift;
+% my ($mod, @fields) = @_;
 package UtilServ::Plugin::<%=$mod%>;
 use parent UtilServ::Plugin::Base;
 use strict;
@@ -10,16 +26,21 @@ use strict;
 #set result output as file download
 __PACKAGE__->modset(output => 'file');
 
-__PACKAGE__->modpageconf(data => 'text');
-
+__PACKAGE__->modpageconf(
+% for my $field (@fields) {
+              <%=$field%> => 'input',
+% }
+            );
 sub proc {
     my $data = $_[1];
-    my ($data) = @{$data}{'data'};
-    $data;
+    my (<%=join ",", map {'$'.$_} @fields%>) = @{$data}{<%=join ",", map {"'$_'"} @fields%>};
+    "";
 };
 T
 
 my $rd = Mojo::Template->new;
-print $rd->render($tp, 'Test');
+open(my $fh, '>', $mod.'.pm') or die "can not open $mod.pm";
+print $fh $rd->render($tp, $mod, @fields);
+close $fh;
 
 1;
